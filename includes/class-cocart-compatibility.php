159891dbfa396f37a766b2f21e786768d7eadbd5
seb86 <mailme@sebastiendumont.com>
2022-03-20 10:10:13 +0100

    First commit

---
+++ includes/class-cocart-compatibility.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * CoCart - Compatibility
+ *
+ * @author  Sébastien Dumont
+ * @package CoCart\Compatibility
+ * @since   3.0.0
+ * @license GPL-2.0+
+ */
+
+namespace CoCart\Compatibility;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Package {
+
+	/**
+	 * Initiate Package.
+	 *
+	 * @access public
+	 */
+	public function init() {
+		self::include_modules();
+	}
+
+
+	/**
+	 * Return the name of the package.
+	 *
+	 * @access public
+	 * @static
+	 * @return string
+	 */
+	public static function get_name() {
+		return 'CoCart Compatibility';
+	} // END get_name()
+
+	/**
+	 * Return the version of the package.
+	 *
+	 * @access public
+	 * @static
+	 * @return string
+	 */
+	public static function get_version() {
+		return self::$version;
+	} // END get_version()
+
+	/**
+	 * Return the path to the package.
+	 *
+	 * @access public
+	 * @static
+	 * @return string
+	 */
+	public static function get_path() {
+		return dirname( __DIR__ );
+	} // END get_path()
+
+	/**
+	 * Load support for extension compatibility.
+	 *
+	 * @access public
+	 */
+	public function include_modules() {
+		include_once dirname( __FILE__ ) . '/modules/class-cocart-advanced-shipping-packages.php'; // Advanced Shipping Packages.
+		include_once dirname( __FILE__ ) . '/modules/class-cocart-free-gift-coupons.php'; // Free Gift Coupons.
+	} // END include_modules()
+
+} // END class.
---
+++ load-package.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is designed to be used to load as package NOT a WP plugin!
+ *
+ * @version 1.0.0
+ * @package CoCart Compatibility Package
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'COCART_COMPATIBILITY_PACKAGE_FILE' ) ) {
+	define( 'COCART_COMPATIBILITY_PACKAGE_FILE', __FILE__ );
+}
+
+// Include the main CoCart Compatibility Package class.
+if ( ! class_exists( 'CoCart\Compatibility\Package', false ) ) {
+	include_once( untrailingslashit( plugin_dir_path( COCART_COMPATIBILITY_PACKAGE_FILE ) ) . '/includes/class-cocart-compatibility.php' );
+}
+
+/**
+ * Returns the main instance of cocart_compatibility_package and only runs if it does not already exists.
+ *
+ * @return cocart_compatibility_package
+ */
+if ( ! function_exists( 'cocart_compatibility_package' ) ) {
+	function cocart_compatibility_package() {
+		return \CoCart\Compatibility\Package::init();
+	}
+
+	cocart_compatibility_package();
+}
